327316ec8d95a50e8e1e6fb04b70f9e82f116515
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-05-31 13:39:53 +0530

    update

--- day16.js
+++ day16.js
@@ -1,4 +1,83 @@
 
+//========================undefined===================================== 
+
+// var vehicles;    //decleration
+// console.log(vehicles)
+
+// vehicles=["car","bus",12345]; ///assign
+// console.log(vehicles)
+
+//==========================Array [   ]============================================
+
+// var studentsname = ["vidhi","pooja","sonu","kiran"];
+// console.log(studentsname)
+
+
+//==============array start counts from 0====================
+
+// var studentsname = ["vidhi","pooja","sonu","kiran"];
+// console.log(studentsname);
+// console.log(studentsname[0]);
+// console.log(studentsname[1]);
+// console.log(studentsname[3]);
+
+
+// ==================.length  start counts from 1=============
+
+// var studentsname = ["vidhi","pooja","sonu","kiran"];
+// console.log(studentsname);
+// console.log(studentsname.length)
+
+
+//=====================loops- for loops====for(){}================================
+// Q. Print numbers from 1- 10;
+
+
+
+// for(){}
+// for (initial condition; ending condition; steps ) { code to be execute }
+// var i = 1;
+// for (i=1;i<=10 ;i++){
+//     console.log(i)
+// }
+
+// var b = 40;
+// for   (b=40;b>=1;b--){
+//     console.log(b)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
