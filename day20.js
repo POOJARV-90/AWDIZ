ee084f468ee8ba1f73460144d4979a918a16ac9d
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-06-04 11:21:46 +0530

    update

---
+++ day20.js
@@ -0,0 +1,70 @@
+
+// var number = 10 ;
+// number++;
+// console.log(number)
+
+
+// Q. Check given number is lesser than 100 or 150 or 200;
+// var number = 500;
+
+// if(number < 100){
+
+//     console.log("number is  less than 100"
+//     )
+   
+// }
+
+// else if (number< 200){
+//     console.log("number is  less than 200"
+//     )
+// }
+
+// else{
+//     console.log("number is not less then 100 , 200 , ")
+// }
+
+// Q. Check user age and provied licennnn... 
+
+// if user age is 18 then he is applicbke for Learnign licc... 
+// user age is greater than 18 and lesser than 80 then dl 
+// user age is lesser 18 or greter 80 not allowed dl  
+
+
+// var canditateage = 10 ;
+
+// if(canditateage == 18  ){
+//     console.log("you are applicable for learning liecence 1")
+// }
+
+// else if(canditateage < 80 && canditateage > 18){
+//     console.log("you are applicable for learning liecence ")
+// }
+
+// else{
+//     console.log("you are not applicable for the liecence")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
