21b6b27b99b8e337d24f208d254bfc229130ebe3
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-06-04 14:22:30 +0530

    Create day22.js

---
+++ day22.js
@@ -0,0 +1,104 @@
+
+
+// var num1 = 2345;
+// var num2 = 8763;
+
+//  function sub (){
+//     var sub = num1 - num2;
+//     console.log(sub)
+
+//  }
+
+//  sub()
+
+
+// function sub(){
+//     var sub = num1-num2;
+
+//     if(sub>0){
+        
+//         return sub;
+
+//     }
+
+//     else if (sub==0){
+
+//         return "sub is greater then 0";
+//     }
+
+//     else{
+
+//         return"sub is not greater then 0"
+//     }
+
+// }
+
+// // var res = sub();
+// console.log(sub())
+// sub()
+
+
+// Q . Find two numbers from given array which addition is equal to target 
+
+// function additionOfNumbers(samiksha, somesh) { // 2 step
+    // console.log(samiksha,somesh,"insdie function")
+   
+//  var target = 105;
+
+//  var array = [23, 345, 565, 65, 40, 40 ,100,5];
+
+
+
+// function addtionofnumber(poo,target){
+
+//     for( var  i= 0 ; i < poo.length -1; i++){
+
+//         for(var j=i+1 ; j < poo.length; j++){
+
+//             console.log("Got It")
+//             if(poo[i] + poo[j] == target){
+//                 console.log(poo[i],poo[j])
+//                 // console.log("Got It")
+//             }
+//         }
+
+
+//     }
+
+
+// }
+
+// addtionofnumber(array,target);
+
+// var num1 = 1234;
+// var num2 = 9876;
+
+// function addition(num1,num2 ){
+//     console.log(num1 + num2)
+// }
+
+// addition(num1,num2)
+
+// Q. Find subtraction of give two numbers by using function. 
+
+var number1 = 9876;
+var number2 = 5678;
+function subtraction(){
+    console.log(num1 - num2)
+}
+
+subtraction(num1,num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
