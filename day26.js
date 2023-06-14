a5907b219cd35eb0c78779cc05a6b1247c3b0bdf
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-06-14 14:45:42 -0700

    day 26

---
+++ day26.js
@@ -0,0 +1,45 @@
+
+
+// setTimeout(()=> {console.log("here after 5sec")},5000)
+
+// setInterval(()=>{console.log("printing after every 2sec")},2000)
+
+
+function plantnauturing() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('growing period');
+        }, 5000);
+    });
+}
+
+async function plantgrowing() {
+    console.log('seed sowing');
+    const result = await plantnauturing();
+    console.log(result,"- through photosynthesis")
+    console.log("fully grown plant :)")
+    // Expected output: "resolved"
+}
+plantgrowing();
+
+// function afterwatering(){
+//     return new Promise ((resolve)) => {
+//         setTimeout(()=>{
+//             resolve('growing');
+//         },2000;
+
+//         )
+//     }
+// }
+
+
+
+
+// function plantgrowing(){
+//     console.log('seed soaing');
+//     const result = await afterwatering();
+//     console.log(result);
+
+// }
+
+// plantgrowing()
