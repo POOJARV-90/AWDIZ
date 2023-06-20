129aa865185be233c6ddca7a358712d94a6a712f
POOJARV-90 <132448302+POOJARV-90@users.noreply.github.com>
2023-06-19 22:29:37 -0700

    update

---
+++ abc.js
@@ -0,0 +1,7 @@
+
+("#nav a").click(function(e){
+    e.preventDefault();
+    (".toggle").hide();
+    var toShow = (this).attr('href');
+    (toShow).show();
+});
---
+++ day29.js
@@ -0,0 +1,3 @@
+
+// const currentime = new Date()
+// console.log(currentime)
